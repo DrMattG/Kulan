a908e1ac9395b61740717a0d0d45d5e7849e7410
Matthew Grainger <matthew.grainger@nina.no>
2021-02-25 17:04:39 +0100

    update pkgdown

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' @title All Flight Right
+#' @title Drone flight Right
 #' @description Dataset of drone flight
 #' @format A data frame with 12670 rows and 18 variables:
 #' \describe{
@@ -23,3 +23,33 @@
 #'}
 #' @source \url{http://somewhere.important.com/}
 "All_Flight_Right"
+
+#' @title Drone flight Left
+#' @description Dataset of drone flight
+#' @format A data frame with 1676 rows and 17 variables:
+#' \describe{
+#'   \item{\code{Photo No.}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Time}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Lat_dec}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Lon_dec}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Alt above the Sea level}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Alt above launch point}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Alt above ground}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Banking angle}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Tangage}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Azimuth}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{GPS Course}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Speed}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{GPS Speed}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Camera position}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Species}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{Number}}{logical COLUMN_DESCRIPTION}
+#'}
+#' @details DETAILS
+"All_Flight_Left"
+
+#' @title DEM model
+#' @description Example of a DEM model
+#' @format RasterLayer
+"DEM30_BK"
--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Kulan
 ![](https://github.com/DrMattG/Kulan/raw/master/man/figures/imgfile.png)
 
-Functions and tools for the kulan project. You can install the package from this repository using the following code
+
+
+
+
+
+
+Functions and tools for the kulan project. 
+
+You can install the package from this repository using the following code: 
 
 devtools::install_github("DrMattG/Kulan")
+
+
+This package represents a workflow that is specific to a particular project and as such might not be generally applicable to other data types. However, if the functions are of use then please feel free to use them.
+
+## Issues
+
+Issues can be raised at https://github.com/DrMattG/Kulan/issues
