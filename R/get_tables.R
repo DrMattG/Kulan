619b2cad154e0e1307ca44d4e8a3eddd9ae742b4
Matthew Grainger <matthew.grainger@nina.no>
2021-03-08 12:58:54 +0100

    change get_tables

--- R/get_tables.R
+++ R/get_tables.R
@@ -4,15 +4,15 @@
 
 get_tables=function(path=path){
 
-file_list=list.files(path =path ,pattern = "_LeftCamera.csv$", recursive = TRUE)
+file_list=list.files(path =path ,pattern = ".csv$", recursive = TRUE)
 
 #initiate a blank data frame, each iteration of the loop will append the data from the given file to this variable
 dataset <- data.frame()
 require(data.table)
 for (i in 1:length(file_list)){
   temp_data <- fread(paste0(path,"/",file_list[i]), stringsAsFactors = F) #read in files using the fread function from the data.table package
-  temp_data$Class <- sapply(strsplit(gsub(".csv", "", file_list[i]), "/"), function(x){x[2]})
-  dataset <- rbindlist(list(dataset, temp_data), use.names = T) #for each iteration, bind the new data to the building dataset
+  temp_data$Class <- gsub(".csv", "", file_list[i])
+  dataset <- rbindlist(list(dataset, temp_data), use.names = T, fill = TRUE) #for each iteration, bind the new data to the building dataset
 }
 dataset
 }
